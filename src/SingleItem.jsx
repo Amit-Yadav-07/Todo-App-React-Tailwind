4c835121edd7f05bedc325f0a5b69c34d2d1b30d
Amit <amitsinghyadav974@gmail.com>
2024-04-17 23:58:58 +0530

    changes

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -5,7 +5,7 @@ let SingleItem = ({ item, removeItem, editItem }) => {
 
 
     return (
-        <div key={item.id} className="h-[auto] w-[90%] bg-red-500 py-2 text-center mx-auto my-2 flex items-center justify-between rounded-md">
+        <div key={item.id} className="h-[auto] w-[90%]  py-2 text-center mx-auto my-2 flex items-center justify-between rounded-md">
             <div className="flex w-[75%]">
                 <input type="checkbox" onChange={() => editItem(item.id)} checked={item.completed} />
                 <strong style={{ textDecoration: item.completed && 'line-through' }} className="uppercase ml-3 tracking-widest	">{item.name}</strong>
