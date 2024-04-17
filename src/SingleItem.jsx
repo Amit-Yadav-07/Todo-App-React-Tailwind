73fbe5735b5bec3b9dd4d5351ec6160bd0f6e3c1
Amit <amitsinghyadav974@gmail.com>
2024-04-17 23:49:28 +0530

    css improve

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -5,7 +5,7 @@ let SingleItem = ({ item, removeItem, editItem }) => {
 
 
     return (
-        <div key={item.id} className="h-[auto] p-2 w-[80%] bg-red-500 py-2 text-center mx-auto my-2 flex items-center justify-between rounded-md">
+        <div key={item.id} className="h-[auto] p-2 w-[80%] py-2 text-center mx-auto my-2 flex items-center justify-between rounded-md">
             <div className="flex w-[75%]">
                 <input type="checkbox" onChange={() => editItem(item.id)} checked={item.completed} />
                 <strong style={{ textDecoration: item.completed && 'line-through' }} className="uppercase ml-3">{item.name}</strong>
